49f45b7cf1598bebb8da94cf9be10c1d286434ca
alidadashzadeh <a.dadashzadeh89@gmail.com>
2026-06-12 13:52:31 -0400

    fix: Suspense error on deploy

--- src/app/signature-pizzas/page.tsx
+++ src/app/signature-pizzas/page.tsx
@@ -2,6 +2,8 @@ import { H2 } from "@/components/ui/Typography";
 import SignaturePizzasList from "@/components/signaturePizzas/SignaturePizzasList";
 
 import { fetchSignaturePizzas } from "@/lib/queries/signaturePizza";
+import { Suspense } from "react";
+import SignatureListLoader from "@/components/ui/SignatureListLoader";
 
 export const revalidate = 300;
 
@@ -11,7 +13,9 @@ export default async function Page() {
 	return (
 		<div className="flex flex-col gap-4">
 			<H2>Discover Signature Pizzas</H2>
-			<SignaturePizzasList pizzas={pizzas} />
+			<Suspense fallback={<SignatureListLoader />}>
+				<SignaturePizzasList pizzas={pizzas} />
+			</Suspense>
 		</div>
 	);
 }
